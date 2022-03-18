209c9c0151a4eabb05ed85ee4230854895b43663
Victor Luca de Mello Rodrigues <victorlukinha@gmail.com>
2022-03-18 19:50:11 -0400

    init repo

---
+++ index.php
@@ -0,0 +1,34 @@
+<html>
+
+<head>
+    <title>Aula de PHP</title>
+</head>
+
+<body>
+   
+
+    <h1>Calcula Média </h1>
+    
+
+    <form action="media.php" method="get">
+
+    <input type="number" name="n1" id="n1">
+    <br>
+    <input type="number" name="n2" id="n2">
+    <br>
+    <input type="number" name="n3" id="n3">
+    <br>
+    <input type="number" name="n4" id="n4">
+    <br>
+    <input type="submit" value="Enviar">
+    <br>
+    <input type="reset" value="Limpar">
+
+    </form>
+
+  
+
+</body>
+
+
+</html>
---
+++ media.php
@@ -0,0 +1,13 @@
+<?php
+
+$media = 0;
+$media = $_GET['n1'] + $_GET['n2'] + $_GET['n3'] +$_GET['n4'];
+
+$media = $media /4 ;
+
+
+echo "Média : "
+echo $media;
+
+
+?>
