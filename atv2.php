d1828fc7d063d59c74c24db93d7b2843a425d947
Victor Luca de Mello Rodrigues <victorlukinha@gmail.com>
2022-03-18 20:03:26 -0400

    atv 2

---
+++ atv2.php
@@ -0,0 +1,46 @@
+<html>
+
+<head>
+    <title>Aula de PHP</title>
+</head>
+
+<body>
+   
+
+    <h1>Dados da Pessoa</h1>
+    
+
+    <form action="pessoa.php" method="get">
+
+    <label for="nome">Nome</label><br>
+    <input type="text" name="nome" id="nome">
+    <br>
+    
+    <label for="cidade">Cidade de Nascimento</label><br>
+    <input type="text" name="cidade" id="cidade">
+    <br>
+    
+    <label for="estado">Estado de Nascimento</label><br>
+    <input type="text" name="estado" id="estado">
+    <br>
+    
+    <label for="dtNasc">Data de Nascimento</label><br>
+    <input type="date" name="dtNasc" id="dtNasc">
+    <br>
+    
+    <label for="end">Endereço</label><br>
+    <input type="text" name="end" id="end">
+    <br>
+    <br>
+    
+    <input type="submit" value="Enviar">
+    <input type="reset" value="Limpar">
+
+    </form>
+
+  
+
+</body>
+
+
+</html>
---
+++ pessoa.php
@@ -0,0 +1,21 @@
+<?php
+
+
+echo (" <h1> nome : " . $_GET['nome'] . "</h1>");
+echo "<br>";
+
+
+echo (" <h1> cidade : " . $_GET['cidade'] . "</h1>");
+echo "<br>";
+
+echo (" <h1> estado : " . $_GET['estado'] . "</h1>");
+echo "<br>";
+
+echo (" <h1> data de nascimento : " . $_GET['dtNasc'] . "</h1>");
+echo "<br>";
+
+echo (" <h1> endereço : " . $_GET['end'] . "</h1>");
+echo "<br>";
+
+
+?>
