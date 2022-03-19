39b182c1618a1430128ca9860f038db3afafeb35
Victor Luca de Mello Rodrigues <victorlukinha@gmail.com>
2022-03-18 20:12:12 -0400

    atv 4

---
+++ atv4.php
@@ -0,0 +1,33 @@
+<html>
+
+<head>
+    <title>Aula de PHP</title>
+</head>
+
+<body>
+   
+
+    <h1>Dados da Pessoa</h1>
+    
+
+    <form action="idade.php" method="get">
+
+    <label for="nome">Nome</label><br>
+    <input type="text" name="nome" id="nome">
+    <br>
+
+    <label for="idade">Idade</label><br>
+    <input type="text" name="idade" id="idade">
+    <br>
+    
+    <input type="submit" value="Enviar">
+    <input type="reset" value="Limpar">
+
+    </form>
+
+  
+
+</body>
+
+
+</html>
---
+++ idade.php
@@ -0,0 +1,13 @@
+<?php
+
+
+echo (" <h1> nome : " . $_GET['nome'] . "</h1>");
+echo "<br>";
+
+
+echo (" <h1> idade : " . $_GET['idade'] . "</h1>");
+echo "<br>";
+
+
+
+?>
