30eb5f28ac09b329cec2e42fce1bf5c2ce798d3f
Victor Luca de Mello Rodrigues <victorlukinha@gmail.com>
2022-03-18 20:08:38 -0400

    atv 3

---
+++ atv3.php
@@ -0,0 +1,50 @@
+<?php
+
+$cidade1 = "Ji-Parana";
+$cidade2 = "PVH";
+$cidade3 = "OPO";
+$cidade4 = "Medice";
+$cidade5 = "Jaru";
+$cidade6 = "Urupa";
+$cidade7 = "Cacoal";
+$cidade8 = "Vilhena";
+$cidade9 = "Colorado";
+$cidade10 = "Buritis";
+
+
+
+
+
+echo (" <h1> Cidade : " . $cidade1 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade2 . "</h1>");
+echo "<br>";
+echo (" <h1> Cidade : " . $cidade3 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade4 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade5 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade6 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade7 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade8 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade9 . "</h1>");
+echo "<br>";
+
+echo (" <h1> Cidade : " . $cidade10 . "</h1>");
+echo "<br>";
+
+
+
+
+?>
